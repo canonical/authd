3v1n0/authd-oidc-brokers

broker: Support Device Authentication without Qr code
We still return the device authentication with qr code rendering by
default, but if authd explicitly tells use that the interface does not
support qr code rendering, then we adapt our strings to that case

internal/broker/authmodes/consts.go
@@ -8,6 +8,9 @@ const (
 	// DeviceID is the ID of the device authentication method.
 	DeviceID = "device_auth"
 
+	// DeviceQrID is the ID of the device authentication method when QrCode rendering is enabled.
+	DeviceQrID = "device_auth_qr"
+
 	// NewPasswordID is the ID of the new password configuration method.
 	NewPasswordID = "newpassword"
 )

internal/broker/broker.go
@@ -219,7 +219,14 @@ func (b *Broker) GetAuthenticationModes(sessionID string, supportedUILayouts []m
 
 	endpoints := make(map[string]struct{})
 	if session.authCfg.provider != nil && session.authCfg.provider.Endpoint().DeviceAuthURL != "" {
-		endpoints[authmodes.DeviceID] = struct{}{}
+		authMode := authmodes.DeviceQrID
+		if _, ok := supportedAuthModes[authMode]; ok {
+			endpoints[authMode] = struct{}{}
+		}
+		authMode = authmodes.DeviceID
+		if _, ok := supportedAuthModes[authMode]; ok {
+			endpoints[authMode] = struct{}{}
+		}
 	}
 
 	availableModes, err := b.providerInfo.CurrentAuthenticationModesOffered(
@@ -261,7 +268,11 @@ func (b *Broker) supportedAuthModesFromLayout(supportedUILayouts []map[string]st
 			if !strings.Contains(layout["wait"], "true") {
 				continue
 			}
-			supportedModes[authmodes.DeviceID] = "Device Authentication"
+			deviceAuthID := authmodes.DeviceQrID
+			if layout["renders_qrcode"] == "false" {
+				deviceAuthID = authmodes.DeviceID
+			}
+			supportedModes[deviceAuthID] = "Device Authentication"
 
 		case "form":
 			if slices.Contains(supportedEntries, "chars_password") {
@@ -312,21 +323,31 @@ func (b *Broker) generateUILayout(session *sessionInfo, authModeID string) (map[
 
 	var uiLayout map[string]string
 	switch authModeID {
-	case authmodes.DeviceID:
+	case authmodes.DeviceID, authmodes.DeviceQrID:
 		ctx, cancel := context.WithTimeout(context.Background(), maxRequestDuration)
 		defer cancel()
 		response, err := session.authCfg.oauth.DeviceAuth(ctx)
 		if err != nil {
-			return nil, fmt.Errorf("could not generate QR code layout: %v", err)
+			return nil, fmt.Errorf("could not generate Device Authentication code layout: %v", err)
 		}
 		session.authInfo["response"] = response
 
-		uiLayout = map[string]string{
-			"type": "qrcode",
-			"label": fmt.Sprintf(
+		var label string
+		if authModeID == authmodes.DeviceQrID {
+			label = fmt.Sprintf(
 				"Scan the QR code or access %q and use the provided login code",
 				response.VerificationURI,
-			),
+			)
+		} else {
+			label = fmt.Sprintf(
+				"Access %q and use the provided login code",
+				response.VerificationURI,
+			)
+		}
+
+		uiLayout = map[string]string{
+			"type":    "qrcode",
+			"label":   label,
 			"wait":    "true",
 			"button":  "Request new login code",
 			"content": response.VerificationURI,
@@ -434,7 +455,7 @@ func (b *Broker) handleIsAuthenticated(ctx context.Context, session *sessionInfo
 
 	var authInfo authCachedInfo
 	switch session.selectedMode {
-	case authmodes.DeviceID:
+	case authmodes.DeviceID, authmodes.DeviceQrID:
 		response, ok := session.authInfo["response"].(*oauth2.DeviceAuthResponse)
 		if !ok {
 			slog.Error("could not get device auth response")

internal/broker/broker_test.go
@@ -273,18 +273,18 @@ func TestSelectAuthenticationMode(t *testing.T) {
 		wantErr bool
 	}{
 		"Successfully select password":    {modeName: authmodes.PasswordID, tokenExists: true},
-		"Successfully select device_auth": {modeName: authmodes.DeviceID},
+		"Successfully select device_auth": {modeName: authmodes.DeviceQrID},
 		"Successfully select newpassword": {modeName: authmodes.NewPasswordID, secondAuthStep: true},
 
 		"Selected newpassword shows correct label in passwd session": {modeName: authmodes.NewPasswordID, passwdSession: true, tokenExists: true, secondAuthStep: true},
 
 		"Error when selecting invalid mode": {modeName: "invalid", wantErr: true},
-		"Error when selecting device_auth but provider is unavailable": {modeName: authmodes.DeviceID, wantErr: true,
+		"Error when selecting device_auth but provider is unavailable": {modeName: authmodes.DeviceQrID, wantErr: true,
 			customHandlers: map[string]testutils.ProviderHandler{
 				"/device_auth": testutils.UnavailableHandler(),
 			},
 		},
-		"Error when selecting device_auth but request times out": {modeName: authmodes.DeviceID, wantErr: true,
+		"Error when selecting device_auth but request times out": {modeName: authmodes.DeviceQrID, wantErr: true,
 			customHandlers: map[string]testutils.ProviderHandler{
 				"/device_auth": testutils.HangingHandler(broker.MaxRequestDuration + 1),
 			},
@@ -524,7 +524,7 @@ func TestIsAuthenticated(t *testing.T) {
 				defer close(firstCallDone)
 
 				if tc.firstMode == "" {
-					tc.firstMode = authmodes.DeviceID
+					tc.firstMode = authmodes.DeviceQrID
 				}
 				updateAuthModes(t, b, sessionID, tc.firstMode)
 
@@ -811,7 +811,7 @@ func TestCancelIsAuthenticated(t *testing.T) {
 	b := newBrokerForTests(t, broker.Config{IssuerURL: provider.URL})
 	sessionID, _ := newSessionForTests(t, b, "", "")
 
-	updateAuthModes(t, b, sessionID, authmodes.DeviceID)
+	updateAuthModes(t, b, sessionID, authmodes.DeviceQrID)
 
 	stopped := make(chan struct{})
 	go func() {

internal/providers/msentraid/msentraid.go
@@ -237,7 +237,9 @@ func (p Provider) CurrentAuthenticationModesOffered(
 		}
 
 	default: // auth mode
-		if _, ok := endpoints[authmodes.DeviceID]; ok && providerReachable {
+		if _, ok := endpoints[authmodes.DeviceQrID]; ok && providerReachable {
+			offeredModes = []string{authmodes.DeviceQrID}
+		} else if _, ok := endpoints[authmodes.DeviceID]; ok && providerReachable {
 			offeredModes = []string{authmodes.DeviceID}
 		}
 		if tokenExists {

internal/providers/noprovider/noprovider.go
@@ -57,7 +57,9 @@ func (p NoProvider) CurrentAuthenticationModesOffered(
 		}
 
 	default: // auth mode
-		if _, ok := endpoints[authmodes.DeviceID]; ok && providerReachable {
+		if _, ok := endpoints[authmodes.DeviceQrID]; ok && providerReachable {
+			offeredModes = []string{authmodes.DeviceQrID}
+		} else if _, ok := endpoints[authmodes.DeviceID]; ok && providerReachable {
 			offeredModes = []string{authmodes.DeviceID}
 		}
 		if tokenExists {

canonical/authd

authd-oidc-brokers/internal/broker/authmodes/consts.go
@@ -8,6 +8,9 @@ const (
 	// Device is the ID of the device authentication method.
 	Device = "device_auth"
 
+	// DeviceQr is the ID of the device authentication method when QrCode rendering is enabled.
+	DeviceQr = "device_auth_qr"
+
 	// NewPassword is the ID of the new password configuration method.
 	NewPassword = "newpassword"
 )

authd-oidc-brokers/internal/broker/broker.go
@@ -219,7 +219,14 @@ func (b *Broker) GetAuthenticationModes(sessionID string, supportedUILayouts []m
 
 	endpoints := make(map[string]struct{})
 	if session.authCfg.provider != nil && session.authCfg.provider.Endpoint().DeviceAuthURL != "" {
-		endpoints[authmodes.Device] = struct{}{}
+		authMode := authmodes.DeviceQr
+		if _, ok := supportedAuthModes[authMode]; ok {
+			endpoints[authMode] = struct{}{}
+		}
+		authMode = authmodes.Device
+		if _, ok := supportedAuthModes[authMode]; ok {
+			endpoints[authMode] = struct{}{}
+		}
 	}
 
 	availableModes, err := b.providerInfo.CurrentAuthenticationModesOffered(
@@ -261,7 +268,11 @@ func (b *Broker) supportedAuthModesFromLayout(supportedUILayouts []map[string]st
 			if !strings.Contains(layout["wait"], "true") {
 				continue
 			}
-			supportedModes[authmodes.Device] = "Device Authentication"
+			deviceAuthID := authmodes.DeviceQr
+			if layout["renders_qrcode"] == "false" {
+				deviceAuthID = authmodes.Device
+			}
+			supportedModes[deviceAuthID] = "Device Authentication"
 
 		case "form":
 			if slices.Contains(supportedEntries, "chars_password") {
@@ -312,21 +323,29 @@ func (b *Broker) generateUILayout(session *sessionInfo, authModeID string) (map[
 
 	var uiLayout map[string]string
 	switch authModeID {
-	case authmodes.Device:
+	case authmodes.Device, authmodes.DeviceQr:
 		ctx, cancel := context.WithTimeout(context.Background(), maxRequestDuration)
 		defer cancel()
 		response, err := session.authCfg.oauth.DeviceAuth(ctx)
 		if err != nil {
-			return nil, fmt.Errorf("could not generate QR code layout: %v", err)
+			return nil, fmt.Errorf("could not generate Device Authentication code layout: %v", err)
 		}
 		session.authInfo["response"] = response
 
-		uiLayout = map[string]string{
-			"type": "qrcode",
-			"label": fmt.Sprintf(
+		label := fmt.Sprintf(
+			"Access %q and use the provided login code",
+			response.VerificationURI,
+		)
+		if authModeID == authmodes.DeviceQr {
+			label = fmt.Sprintf(
 				"Scan the QR code or access %q and use the provided login code",
 				response.VerificationURI,
-			),
+			)
+		}
+
+		uiLayout = map[string]string{
+			"type":    "qrcode",
+			"label":   label,
 			"wait":    "true",
 			"button":  "Request new login code",
 			"content": response.VerificationURI,
@@ -434,7 +453,7 @@ func (b *Broker) handleIsAuthenticated(ctx context.Context, session *sessionInfo
 
 	var authInfo authCachedInfo
 	switch session.selectedMode {
-	case authmodes.Device:
+	case authmodes.Device, authmodes.DeviceQr:
 		response, ok := session.authInfo["response"].(*oauth2.DeviceAuthResponse)
 		if !ok {
 			slog.Error("could not get device auth response")

authd-oidc-brokers/internal/broker/broker_test.go
@@ -273,18 +273,18 @@ func TestSelectAuthenticationMode(t *testing.T) {
 		wantErr bool
 	}{
 		"Successfully select password":    {modeName: authmodes.Password, tokenExists: true},
-		"Successfully select device_auth": {modeName: authmodes.Device},
+		"Successfully select device_auth": {modeName: authmodes.DeviceQr},
 		"Successfully select newpassword": {modeName: authmodes.NewPassword, secondAuthStep: true},
 
 		"Selected newpassword shows correct label in passwd session": {modeName: authmodes.NewPassword, passwdSession: true, tokenExists: true, secondAuthStep: true},
 
 		"Error when selecting invalid mode": {modeName: "invalid", wantErr: true},
-		"Error when selecting device_auth but provider is unavailable": {modeName: authmodes.Device, wantErr: true,
+		"Error when selecting device_auth but provider is unavailable": {modeName: authmodes.DeviceQr, wantErr: true,
 			customHandlers: map[string]testutils.ProviderHandler{
 				"/device_auth": testutils.UnavailableHandler(),
 			},
 		},
-		"Error when selecting device_auth but request times out": {modeName: authmodes.Device, wantErr: true,
+		"Error when selecting device_auth but request times out": {modeName: authmodes.DeviceQr, wantErr: true,
 			customHandlers: map[string]testutils.ProviderHandler{
 				"/device_auth": testutils.HangingHandler(broker.MaxRequestDuration + 1),
 			},
@@ -524,7 +524,7 @@ func TestIsAuthenticated(t *testing.T) {
 				defer close(firstCallDone)
 
 				if tc.firstMode == "" {
-					tc.firstMode = authmodes.Device
+					tc.firstMode = authmodes.DeviceQr
 				}
 				updateAuthModes(t, b, sessionID, tc.firstMode)
 
@@ -811,7 +811,7 @@ func TestCancelIsAuthenticated(t *testing.T) {
 	b := newBrokerForTests(t, broker.Config{IssuerURL: provider.URL})
 	sessionID, _ := newSessionForTests(t, b, "", "")
 
-	updateAuthModes(t, b, sessionID, authmodes.Device)
+	updateAuthModes(t, b, sessionID, authmodes.DeviceQr)
 
 	stopped := make(chan struct{})
 	go func() {

authd-oidc-brokers/internal/providers/msentraid/msentraid.go
@@ -237,7 +237,9 @@ func (p Provider) CurrentAuthenticationModesOffered(
 		}
 
 	default: // auth mode
-		if _, ok := endpoints[authmodes.Device]; ok && providerReachable {
+		if _, ok := endpoints[authmodes.DeviceQr]; ok && providerReachable {
+			offeredModes = []string{authmodes.DeviceQr}
+		} else if _, ok := endpoints[authmodes.Device]; ok && providerReachable {
 			offeredModes = []string{authmodes.Device}
 		}
 		if tokenExists {

authd-oidc-brokers/internal/providers/noprovider/noprovider.go
@@ -57,7 +57,9 @@ func (p NoProvider) CurrentAuthenticationModesOffered(
 		}
 
 	default: // auth mode
-		if _, ok := endpoints[authmodes.Device]; ok && providerReachable {
+		if _, ok := endpoints[authmodes.DeviceQr]; ok && providerReachable {
+			offeredModes = []string{authmodes.DeviceQr}
+		} else if _, ok := endpoints[authmodes.Device]; ok && providerReachable {
 			offeredModes = []string{authmodes.Device}
 		}
 		if tokenExists {