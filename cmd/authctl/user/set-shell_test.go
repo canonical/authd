ca31e9c00821c60d4d537045f0d0f5434595425d
Adrian Dombeck <adrian.dombeck@canonical.com>
2025-12-15 17:55:45 +0100

    Add `authctl user set-shell` command

---
+++ cmd/authctl/user/set-shell_test.go
@@ -0,0 +1,48 @@
+package user_test
+
+import (
+	"os"
+	"os/exec"
+	"path/filepath"
+	"testing"
+
+	"github.com/canonical/authd/internal/testutils"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc/codes"
+)
+
+func TestSetShellCommand(t *testing.T) {
+	t.Parallel()
+
+	daemonSocket := testutils.StartAuthd(t, daemonPath,
+		testutils.WithGroupFile(filepath.Join("testdata", "empty.group")),
+		testutils.WithPreviousDBState("one_user_and_group"),
+		testutils.WithCurrentUserAsRoot,
+	)
+
+	err := os.Setenv("AUTHD_SOCKET", daemonSocket)
+	require.NoError(t, err, "Failed to set AUTHD_SOCKET environment variable")
+
+	tests := map[string]struct {
+		args []string
+
+		expectedExitCode int
+	}{
+		"Set_shell_success": {args: []string{"set-shell", "user1", "/bin/bash"}, expectedExitCode: 0},
+
+		"Error_when_user_does_not_exist": {
+			args:             []string{"set-shell", "invaliduser", "/bin/bash"},
+			expectedExitCode: int(codes.NotFound),
+		},
+	}
+
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			//nolint:gosec // G204 it's safe to use exec.Command with a variable here
+			cmd := exec.Command(authctlPath, append([]string{"user"}, tc.args...)...)
+			testutils.CheckCommand(t, cmd, tc.expectedExitCode)
+		})
+	}
+}
