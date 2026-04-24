f87a09fc959fa25b39a8f168b992f6b48e9941dd
Adrian Dombeck <adrian.dombeck@canonical.com>
2026-04-24 11:02:54 +0200

    e2e-tests/vm: Fix ssh script called without release / VM name
    Ensure that VM_NAME is exported to the environment of the ssh script to
    avoid
    
      Error: Missing required argument <release>

--- e2e-tests/vm/provision-authd.sh
+++ e2e-tests/vm/provision-authd.sh
@@ -141,7 +141,7 @@ assert_env_vars RELEASE BROKER
 ARTIFACTS_DIR="${ARTIFACTS_DIR:-${DATA_DIR}/${RELEASE}}"
 
 if [ -z "${VM_NAME:-}" ]; then
-    VM_NAME="${VM_NAME_BASE}-${RELEASE}"
+    export VM_NAME="${VM_NAME_BASE}-${RELEASE}"
 fi
 export VM_NAME
 
