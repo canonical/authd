eebb345da871983d7fbde692eebb53f764708211
Marco Trevisan (Treviño) <mail@3v1n0.net>
2026-05-15 12:26:54 +0200

    pam/go-exec: Use PAM_TTY if set to check if we are in interactive mode
    To ensure that we are in interactive mode we should rely on PAM_TTY if
    set, otherwise just check stdin/stderr as before.
    
    Closes: #901

--- pam/go-exec/module.c
+++ pam/go-exec/module.c
@@ -980,6 +980,7 @@ do_pam_action_thread (pam_handle_t *pamh,
   g_autofree char *log_file = NULL;
   g_autofree char *program_name = NULL;
   g_autofree char *wait_thread_name = NULL;
+  g_autofd int pam_tty_fd = -1;
   g_autofd int stdin_fd = -1;
   g_autofd int stdout_fd = -1;
   g_autofd int stderr_fd = -1;
@@ -1093,19 +1094,43 @@ do_pam_action_thread (pam_handle_t *pamh,
   main_context = g_main_context_ref (module_data->main_context);
   context_pusher = g_main_context_pusher_new (main_context);
 
-  interactive_mode = isatty (STDIN_FILENO);
+  stdin_fd = STDIN_FILENO;
+  stdout_fd = STDOUT_FILENO;
+
+  const char *pam_tty;
+  if ((exit_status = pam_get_item (pamh, PAM_TTY, (const void **) &pam_tty)) != PAM_SUCCESS)
+    return exit_status;
+
+  if (pam_tty != NULL && *pam_tty != '\0')
+    {
+      g_debug ("Trying to use PAM TTY %s", pam_tty);
+      pam_tty_fd = open (pam_tty, O_RDWR, 0600);
+      errsv = errno;
+
+      if (pam_tty_fd >= 0)
+        {
+          stdin_fd = pam_tty_fd;
+          stdout_fd = pam_tty_fd;
+        }
+      else
+        {
+          g_debug ("Impossible to open PAM_TTY %s: %s", pam_tty, g_strerror (errsv));
+        }
+    }
+
+  interactive_mode = isatty (stdin_fd) && isatty (stdout_fd);
   g_debug ("Running in interactive mode: %s", interactive_mode ? "true" : "false");
 
   if (interactive_mode)
     {
-      if ((stdin_fd = dup_fd_checked (STDIN_FILENO, &error)) < 0)
+      if ((stdin_fd = dup_fd_checked (stdin_fd, &error)) < 0)
         {
           notify_error (pamh, action, "can't duplicate stdin file descriptor: %s",
                         error->message);
           return PAM_SYSTEM_ERR;
         }
 
-      if ((stdout_fd = dup_fd_checked (STDOUT_FILENO, &error)) < 0)
+      if ((stdout_fd = dup_fd_checked (stdout_fd, &error)) < 0)
         {
           notify_error (pamh, action, "can't duplicate stdout file descriptor: %s",
                         error->message);
@@ -1118,6 +1143,11 @@ do_pam_action_thread (pam_handle_t *pamh,
                         error->message);
           return PAM_SYSTEM_ERR;
         }
+    }
+  else
+    {
+      g_steal_fd (&stdin_fd);
+      g_steal_fd (&stdout_fd);
     }
 
   action_data.connection_new_id =
