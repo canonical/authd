03d1f32635a736f53e4836c1b40dea399d8c8faf
Shane Crowley <66971213+edibotopic@users.noreply.github.com>
2026-06-15 13:57:59 +0100

    remove redundant f-string
    Co-authored-by: Noor Eldeen Mansour <nooreldeen.mansour@canonical.com>

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ import yaml
 project = "authd"
 author = "Canonical Ltd."
 
-version = f"{os.environ.get('READTHEDOCS_VERSION', 'local')}"
+version = os.environ.get('READTHEDOCS_VERSION', 'local')
 
 # Sidebar documentation title; best kept reasonably short
 #
